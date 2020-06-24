5174a6519bd2e5e99501a5b550107d0a193ec939
lafry5 <lafry5@yahoo.com>
2020-06-24 08:59:51 -0700

    stackable updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,9 +140,64 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
-$(".card .list-group").sortable({
-  connectWith: $(".card .list-group")
-}); // drag and drop
+$(".card .list-group").sortable({ //is this the right code?
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+  $(this).children().each(function() {
+  console.log($(this));
+  //array to store the task data in 
+  //var tempArr = [];
+
+    // Do I keep this code?
+    // loop over current set of children in sortable list
+      $(this).children().each(function() {
+        var text = $(this)
+          .find("p")
+          .text()
+          .trim();
+
+        var date = $(this)
+          .find("span")
+          .text()
+          .trim();
+
+        //add task data to the temp array as an object
+       // tempArr.Arr.push({
+       //   text: text,
+       //   date: date
+       // });
+      });
+
+    //  console.log(tempArr); 
+      console.log(text, date);
+ //     });
+    });
+  }
+}); //drag and drop
+
+  // trim down list's ID to match object property
+  //var arrName = $(this)
+  //  .attr("id")
+ //   .replace("list-", "");
+
+  // update array on tasks object and save
+ // tasks[arrName] = tempArr;
+ // saveTasks();
 
 
 // modal was triggered
